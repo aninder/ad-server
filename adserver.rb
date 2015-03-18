107c1cdab8a15f795167c23a52c0dea5fd20c144
aninder <aninder@gmail.com>
2015-03-18 14:56:38 +0530

    added ad create action

--- adserver.rb
+++ adserver.rb
@@ -21,11 +21,46 @@ class Ad
   property :updated_at,   DateTime
   property :size,         Integer
   property :content_type, String
-
 end
 
+DataMapper::Model.raise_on_save_failure = true
 DataMapper.auto_upgrade!
 
-before do
-  headers "Content-Type"  => "text/html; charset=utf-8"
+get "/" do
+  @page_title="default page"
+  erb :landing_page
 end
+get "/new" do
+  @page_title="Create a New Ad"
+  erb :new
+end
+
+post "/create" do
+  ad = Ad.new(params[:ad])
+  ad.content_type = params[:image][:type]
+  ad.size = File.size(params[:image][:tempfile])
+  ad.filename = params[:image][:filename]
+  if ad.save
+    upload_file = params[:image][:tempfile];
+    path_to_save = File.join(Dir.pwd, "/public/ads",ad.filename);
+    File.open(path_to_save,"wb") do |f|
+      f.write(upload_file.read)
+    end
+    redirect("/show/#{ad.id}");
+  else
+    redirect('/list');
+  end
+end
+
+get "show/:id" do
+end
+
+get "/ad" do
+end
+get "/list" do
+end
+get "delete/:id" do
+end
+get "click/:id" do
+end
+
